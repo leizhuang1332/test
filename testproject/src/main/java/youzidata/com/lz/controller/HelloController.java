fc99d15847269c6662e15c03dd07548b19fd31d8
leizhuang1332 <47847171+leizhuang1332@users.noreply.github.com>
2019-11-12 09:41:35 +0800

    2
    2

--- testproject/src/main/java/youzidata/com/lz/controller/HelloController.java
+++ testproject/src/main/java/youzidata/com/lz/controller/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
     @GetMapping("helloworld")
     public String helloWorld(){
         // System.out.println("1");
+        // System.out.println("2");
 
         return "hello 哈哈哈 3.0 完美自动部署 git 上多层目录构建  ";
     }
